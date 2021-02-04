a87f71b0bf6e4c84ed7ef53737035f5cd74147cc
dgoudie <34222208+dgoudie@users.noreply.github.com>
2021-02-03 23:19:32 -0600

    removed graphql

--- index.ts
+++ index.ts
@@ -1,102 +1,49 @@
-import 'reflect-metadata';
-
 import { Elements, HandleProps } from 'react-flow-renderer';
-import { Field, ObjectType, Query, Resolver } from 'type-graphql';
 
-@ObjectType()
-export class Info {
-    @Field()
+export interface Info {
     name: string;
-    @Field()
     title: string;
-    @Field()
     links: Link[];
-    @Field()
     about: string[];
-    @Field()
     education: EducationItem[];
-    @Field()
     workExperience: WorkExperienceItem[];
-    @Field()
     interests: string[];
-    @Field()
     topSkills: TopSkillItem[];
-    @Field()
     websiteStackElements: Elements<WebsiteStackElementData>;
 }
-@ObjectType()
-export class Link {
-    @Field()
+export interface Link {
     text: string;
-
-    @Field()
     link: string;
-
-    @Field()
     faIcon: string;
-
-    @Field()
     textForPrint: string;
-
-    @Field()
     name: string;
 }
-@ObjectType()
-export class EducationItem {
-    @Field()
+export interface EducationItem {
     iconUrl: string;
-
-    @Field()
     title: string;
-
-    @Field()
     secondaryInfo: string[];
 }
-@ObjectType()
-export class WorkExperienceItem {
-    @Field()
+export interface WorkExperienceItem {
     iconUrl: string;
-
-    @Field()
     company: string;
-
-    @Field()
     jobTitle: string;
-
-    @Field()
     startDate: Date;
-
-    @Field()
     endDate: Date;
-
-    @Field()
     description: string;
 }
-@ObjectType()
-export class TopSkillItem {
-    @Field()
+export interface TopSkillItem {
     name: string;
-
-    @Field()
     percentage: number;
 }
-@ObjectType()
-export class BuiltWithItem {
-    @Field()
+export interface BuiltWithItem {
     iconUrl: string;
-    @Field()
     name: string;
-    @Field({ nullable: true })
     links?: BuiltWithItemLink[];
 }
-@ObjectType()
-export class BuiltWithItemLink {
-    @Field()
+export interface BuiltWithItemLink {
     text: string;
-    @Field()
     url: string;
 }
-@ObjectType()
-export class WebsiteStackElementData {
+export interface WebsiteStackElementData {
     handles: HandleProps[];
 }
