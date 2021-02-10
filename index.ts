8bf5a8619516cb4c398d181ffcf0a9db3d9fe3a8
dgoudie <34222208+dgoudie@users.noreply.github.com>
2021-02-10 09:41:33 -0600

    made item info a class

--- index.ts
+++ index.ts
@@ -36,9 +36,14 @@ export interface TopSkillItem {
     percentage: number;
 }
 
-export interface WebsiteStackItemInfo {
+export class WebsiteStackItemInfo {
     title: string;
     imageUrl: string;
+
+    constructor(props: WebsiteStackItemInfo) {
+        this.title = props.title;
+        this.imageUrl = props.imageUrl;
+    }
 }
 export interface WebsiteStackGraphElementData extends WebsiteStackItemInfo {
     handles?: HandleProps[];
