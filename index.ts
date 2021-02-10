0bb04e8edce9e251b460262c367122f3db064002
dgoudie <34222208+dgoudie@users.noreply.github.com>
2021-02-10 10:28:16 -0600

    added more properties to WebsiteStackItemInfo

--- index.ts
+++ index.ts
@@ -39,11 +39,22 @@ export interface TopSkillItem {
 export class WebsiteStackItemInfo {
     title: string;
     imageUrl: string;
+    description: string;
+    links?: WebsiteStackItemInfoLink[];
+    additionalImageUrls?: string[];
 
     constructor(props: WebsiteStackItemInfo) {
         this.title = props.title;
         this.imageUrl = props.imageUrl;
+        this.description = props.description;
+        this.links = props.links;
+        this.additionalImageUrls = props.additionalImageUrls;
     }
+}
+
+export interface WebsiteStackItemInfoLink {
+    text: string;
+    href: string;
 }
 export interface WebsiteStackGraphElementData extends WebsiteStackItemInfo {
     handles?: HandleProps[];
